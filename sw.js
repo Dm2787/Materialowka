28126227e2cc00c4ea0c1721fb4ce2a703a24588
Dm2787 <damianmuttke@gmail.com>
2026-05-22 20:00:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = 'materialowka-v2'; // Zmiana tej wersji wymusi pobranie nowej ikony
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-192.png', // Upewnij się, że nazwy plików ikon zgadzają się z Twoimi
+    './icon-512.png'
+];
 
+// Instalacja Service Workera i buforowanie plików
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        }).then(() => self.skipWaiting())
+    );
+});
+
+// Aktywacja i czyszczenie starych wersji cache
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// Serwowanie plików z cache w trybie offline
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(cachedResponse => {
+            return cachedResponse || fetch(e.request);
+        })
+    );
+});
+                           
