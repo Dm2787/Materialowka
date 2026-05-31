7d5b4386c0c9e4f9e29f5b6d73c8873ba744f0a8
Dm2787 <damianmuttke@gmail.com>
2026-05-31 17:01:21 +0200

    Change cache version from v16 to v17
    Updated cache version to force new icon retrieval.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'materialowka-v16'; // Zmiana tej wersji wymusi pobranie nowej ikony
+const CACHE_NAME = 'materialowka-v17'; // Zmiana tej wersji wymusi pobranie nowej ikony
 const ASSETS = [
     './',
     './index.html',
